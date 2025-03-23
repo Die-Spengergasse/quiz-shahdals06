209d59f0025489645984589411dbf67408444df4
shahd <als220003@spengergasse.at>
2025-03-23 15:55:20 +0100

    --

--- src/main/java/at/spengergasse/Main.java
+++ src/main/java/at/spengergasse/Main.java
@@ -3,7 +3,6 @@ package at.spengergasse;
 import at.spengergasse.entities.Answer;
 import at.spengergasse.entities.UserResponse;
 import at.spengergasse.entities.Question;
-import at.spengergasse.entities.UserResponse;
 
 import javax.persistence.*;
 import java.util.List;
@@ -14,63 +13,59 @@ public class Main {
         EntityManager em = Persistence.createEntityManagerFactory("demo")
                 .createEntityManager();
 
-
-
         Scanner scanner = new Scanner(System.in);
 
         TypedQuery<Question> queryQue = em.createQuery("SELECT q from Question q", Question.class);
         List<Question> questions = queryQue.getResultList();
 
-        char[] answerChar = new char[4];
-        answerChar = new char[]{'a', 'b', 'c', 'd'};
+        char[] answerChar = new char[]{'a', 'b', 'c', 'd'};
 
         System.out.println("___________Quiz___________");
 
-        UserResponse user = new UserResponse();
-        String input = user.getAnswer();
-        String selection = "Selection: ";
-        String correct = "Correct: ";
-
-
+        int correctAnswersCount = 0;  // Richtige Antworten zählen
+        int totalQuestions = questions.size(); // Gesamtzahl der Fragen
 
         for (Question q : questions) {
-
-            String rightAnswer = " ";
+            String rightAnswer = "";
             List<Answer> answers = q.getAnswers();
             System.out.println(q.getText());
-            System.out.println(selection);
-            for (int i = 0; i < q.getAnswers().size(); i++) {
+            System.out.println("Selection: ");
+
+            for (int i = 0; i < answers.size(); i++) {
                 System.out.println(answerChar[i] + ") " + answers.get(i).getText());
                 if (answers.get(i).isCorrect()) {
                     rightAnswer = String.valueOf(answerChar[i]);
-                    correct += rightAnswer + ") " + answers.get(i).getText();
                 }
-
             }
 
             boolean correctInput = false;
+            String input = "";
             while (!correctInput) {
                 System.out.print("Your Answer: ");
-                input = (scanner.next());
+                input = scanner.next();
                 if (!input.matches("[a-d]")) {
                     System.out.println("Bitte nur a - d eingeben!");
-                }
-                else
-                {
+                } else {
                     correctInput = true;
                 }
             }
-            if (!input.equals(rightAnswer)) {
-                System.out.println(correct);
-                System.out.println("\n--next Question--");
-            }
-            else
-            {
-                System.out.println("\n--next Question--");
-            }
-            correct = "Correct: ";
 
+            if (input.equals(rightAnswer)) {
+                correctAnswersCount++;  // Erhöht die Anzahl der richtigen Antworten
+            } else {
+                System.out.println("Falsche Antwort! Die richtige Antwort war: " + rightAnswer);
+            }
+
+            System.out.println("\n--next Question--");
         }
+
+        // Ergebnisberechnung
+        double percentage = ((double) correctAnswersCount / totalQuestions) * 100;
+
+        System.out.println("\n### Quiz beendet ###");
+        System.out.println("Richtige Antworten: " + correctAnswersCount + " von " + totalQuestions);
+        System.out.printf("Erfolgsquote: %.2f%%\n", percentage);
+
         em.close();
     }
 }
